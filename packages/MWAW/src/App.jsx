008429d79c00a6237c7624c7879e710c858d2905
MWAW Dev2 <dev2@marvelousworkandawonder.com>
2018-01-24 16:17:59 -0700

    (feat) Add navigation bar

--- packages/MWAW/src/App.jsx
+++ packages/MWAW/src/App.jsx
@@ -1,7 +1,55 @@
 import React from 'react';
+import styled from 'styled-components';
 
+const Button = styled.button`
+  display: inline-block;
+  color: #FFFBFA;
+  background: #666;
+  border: none;
+  padding: 0.5rem;
+  width: 10rem;
+  margin: 0.5rem;
+  text-align: center;
+`
+const Menu = styled.div`
+
+`
+const Nav = styled.div`
+  background: #666;
+  opacity: 0.7;
+  width: 100%;
+  padding: 0.5em;
+  margin: 0em;
+  height: 45px;
+  clear: right;
+  z-index: 99;
+`
+const Search = styled.input`
+  display: inline;
+  color: #666;
+  background: #FFFBFA;
+  opacity: 0.8;
+  border: 1px solid #666;
+  padding: 0.5rem;
+  width: 8rem;
+  margin: 0.5rem;
+  text-align: left;
+  position: fixed;
+  float: right;
+  overflow: none;
+  z-index: 100;
+`
 export default function App() {
   return (
-    <div>App</div>
+    <Nav>
+      <div>
+        <Button>Login</Button>
+        <Search
+        placeholder=" Search"
+        innerRef={x => { this.input = x }}
+        onMouseEnter={() => this.input.focus()}
+      />
+      </div>
+    </Nav>
   );
 }
