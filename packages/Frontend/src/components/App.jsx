6d9a8352d432b0243d903dfc3572561368e00a2e
MWAW Developer <dev@marvelousworkandawonder.com>
2018-02-03 10:28:40 -0700

    (refactor) move App logic to AppContainer

--- packages/Frontend/src/components/App.jsx
+++ packages/Frontend/src/components/App.jsx
@@ -1,23 +1,23 @@
-import React, { Fragment, Component } from 'react';
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
 import { Button } from 'semantic-ui-react';
 import SidebarLeftOverlay from './SidebarLeftOverlay';
 import SidebarTopOverlay from './SidebarTopOverlay';
 
-export default class App extends Component {
-  state = { visible: false }
-
-  toggleVisibility = () => this.setState({ visible: !this.state.visible })
-
-  render() {
-    return (
-      <Fragment>
-        <SidebarTopOverlay toggleMenu={this.toggleVisibility} />
-        <SidebarLeftOverlay
-          sideBarVisibility={this.state.visible}
-          toggleMenu={this.toggleVisibility}
-        />
-        <Button>Real Truth</Button>
-      </Fragment>
-    );
-  }
+export default function App({ toggleVisibility, visible }) {
+  return (
+    <Fragment>
+      <SidebarTopOverlay toggleMenu={toggleVisibility} />
+      <SidebarLeftOverlay
+        sideBarVisibility={visible}
+        toggleMenu={toggleVisibility}
+      />
+      <Button>Real Truth</Button>
+    </Fragment>
+  );
 }
+
+App.propTypes = {
+  toggleVisibility: PropTypes.func.isRequired,
+  visible: PropTypes.bool.isRequired,
+};
