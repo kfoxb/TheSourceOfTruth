acf01e3132a5d1cda0a26cd84a971723f22d3b78
mwawdev2 <34819339+mwawdev2@users.noreply.github.com>
2018-02-03 11:01:40 -0700

    (feat) add side menu (#19)
    * (feat) Add initial navigation bar
    
    * (feat) Add side menu
    
    * (feat) gitignored DS_Store
    
    * (feat) Open menu on top menu content button click
    
    * (feat) add env browser to eslintrc
    
    * (feat) close side menu on offclick
    
    * (test) Remove unneeded test
    
    * (fix) remove Router basename
    
    * (refactor) move App logic to AppContainer
    
    * (refactor) delete unused components and files
    
    * (refactor) move SidebarTopOverlay & SidebarLeftOverlay to Navigation

--- packages/Frontend/.eslintrc.js
+++ packages/Frontend/.eslintrc.js
@@ -1,5 +1,8 @@
 module.exports = {
   extends: 'airbnb',
+  "env": {
+    "browser": true,
+  },
   parser: 'babel-eslint',
   rules: {
     "strict": 0
--- packages/Frontend/src/containers/AppContainer.jsx
+++ packages/Frontend/src/containers/AppContainer.jsx
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import App from '../components/App';
 
-export default function AppContainer() {
-  return (<App />);
+export default class AppContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: false,
+    };
+  }
+
+  toggleVisibility = () => this.setState({ visible: !this.state.visible })
+
+  render() {
+    return (
+      <App
+        toggleVisibility={this.toggleVisibility}
+        visible={this.state.visible}
+      />);
+  }
 }
