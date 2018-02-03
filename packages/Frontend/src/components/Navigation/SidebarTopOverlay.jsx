1d297cff99814ef592af457a5f81724b862f522e
mwawdev2 <34819339+mwawdev2@users.noreply.github.com>
2018-02-03 11:45:19 -0700

    (feat) Change search bar to search icon (#21)
    * (feat) Change search bar to search icon
    
    * (fix) Remove unnecessary prop

--- packages/Frontend/src/components/Navigation/SidebarTopOverlay.jsx
+++ packages/Frontend/src/components/Navigation/SidebarTopOverlay.jsx
@@ -1,11 +1,11 @@
 import React, { Fragment } from 'react';
-import { Search, Sidebar, Menu, Icon } from 'semantic-ui-react';
+import { Icon, Menu, Sidebar } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 
 export default function SidebarTopOverlay({ toggleMenu }) {
   return (
     <Fragment>
-      <Sidebar as={Menu} animation="overlay" direction="top" visible inverted>
+      <Sidebar animation="overlay" as={Menu} direction="top" inverted visible >
         <Menu.Item name="Menu" onClick={toggleMenu}>
           <Icon name="content" />
         </Menu.Item>
@@ -19,7 +19,9 @@ export default function SidebarTopOverlay({ toggleMenu }) {
         <Menu.Item name="Sign in">
           <Icon name="user" />
         </Menu.Item>
-        <Search />
+        <Menu.Item name="Search">
+          <Icon name="search" />
+        </Menu.Item>
       </Sidebar>
     </Fragment>
   );
