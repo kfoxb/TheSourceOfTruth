e4d2bcc0b9df1a7e64fc9732315a65d01b6899c4
MWAW Developer <dev@marvelousworkandawonder.com>
2018-02-19 16:54:12 -0700

    (feat) add a username and password field to signin component

--- packages/Frontend/src/components/SignIn.jsx
+++ packages/Frontend/src/components/SignIn.jsx
@@ -1,5 +1,37 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
+import { Auth } from 'aws-amplify';
 
-export default function SignIn() {
-  return (<h3>SignIn</h3>);
+export default class SignIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '', // eslint-disable-line react/no-unused-state
+      password: '', // eslint-disable-line react/no-unused-state
+      error: '',
+    };
+  }
+
+  updateUsername = ({ target }) => { this.setState({ username: target.value }); }
+  updatePassword = ({ target }) => { this.setState({ password: target.value }); }
+
+  signin = () => {
+    const { username, password } = this.state;
+    Auth.signIn(username, password)
+      .then(user => console.log(user))
+      .catch(error => this.setState({ error }));
+  }
+
+  render() {
+    const { error } = this.state;
+    return (
+      <Fragment>
+        <p>Username: </p>
+        <input onChange={this.updateUsername} />
+        <p>Password: </p>
+        <input type="password" onChange={this.updatePassword} />
+        <button disabled={this.state.password.length < 8}onClick={this.signin}>Submit</button>
+        { error && error.message ? error.message : null }
+      </Fragment>
+    );
+  }
 }
