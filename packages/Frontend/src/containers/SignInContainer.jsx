d46835ecb73eeae7e9739ec0f4b0384432e1c127
MWAW Developer <dev@marvelousworkandawonder.com>
2018-02-21 20:00:36 -0700

    (fix) remove console.log

--- packages/Frontend/src/containers/SignInContainer.jsx
+++ packages/Frontend/src/containers/SignInContainer.jsx
@@ -14,7 +14,6 @@ class SignInContainer extends Component {
 
   constructor(props) {
     super(props);
-    console.log('this is props', props);
     this.state = {
       error: '',
       loading: false,
