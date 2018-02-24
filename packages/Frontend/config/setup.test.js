f2385015c0834bd8043299c301590f3af54ce587
MWAW Developer <dev@marvelousworkandawonder.com>
2018-02-24 09:52:03 -0700

    (feat) install and configure jest

--- packages/Frontend/config/setup.test.js
+++ packages/Frontend/config/setup.test.js
@@ -2,6 +2,3 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
-
-const context = require.context('../src', true, /\.test\.jsx?$/);
-context.keys().forEach(context);
