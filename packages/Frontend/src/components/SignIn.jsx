b2455d9247410c5c3fa7d22717cf67bb8cd876de
MWAWDev2 <dev2@marvelousworkandawonder.com>
2018-03-01 10:51:28 -0700

    (fix) Center SignIn component, add colors to button, add Sign In header, and add Icon

--- packages/Frontend/src/components/SignIn.jsx
+++ packages/Frontend/src/components/SignIn.jsx
@@ -1,19 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button, Divider, Form, Segment, Loader, Message } from 'semantic-ui-react';
+import { Button, Divider, Form, Header, Icon, Loader, Segment, Message } from 'semantic-ui-react';
 
 export default function SignIn({
   error, loading, signIn, submitIfEnter, updateFormByKey,
 }) {
   return (
-    <div>
+    <div style={{
+        margin: 'auto',
+        position: 'relative',
+        top: 100,
+        width: '50%',
+      }}
+    >
       <Segment>
+        <Header as="h1" icon textAlign="center">
+          <Icon name="user" circular />
+          <Header.Content>
+            Sign In
+          </Header.Content>
+        </Header>
         <Form>
           <Form.Input placeholder="Username" onChange={updateFormByKey('username')} onKeyUp={submitIfEnter} />
           <Form.Input placeholder="Password" onChange={updateFormByKey('password')} onKeyUp={submitIfEnter} type="password" />
-          <Button onClick={signIn} fluid>Sign In</Button>
+          <Button color="violet" onClick={signIn} fluid>Sign In</Button>
           <Divider horizontal>Or</Divider>
-          <Button fluid>Sign Up Now</Button>
+          <Button color="blue" fluid>Sign Up Now</Button>
         </Form>
         { loading && <Loader active inline /> }
         { error &&
