987c3fefd751f92597887844f7be11d2df8a54d1
MWAW Developer <dev@marvelousworkandawonder.com>
2018-03-03 10:26:48 -0700

    (test) add tests for SignInContainer

---
+++ packages/Frontend/src/containers/__mocks__/aws-amplify.js
@@ -0,0 +1,18 @@
+export class Auth {
+  username = 'user'
+
+  password = 'password'
+
+  signIn(username, password) {
+    return new Promise((resolve, reject) => {
+      if (username === this.username && password === this.password) {
+        return resolve();
+      }
+      return reject();
+    });
+  }
+
+  currentAuthenticatedUser() {
+    return Promise.resolve({ username: this.username });
+  }
+}
