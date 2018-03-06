1e368acbfa5150910a51bd228a841eba2e7418a6
MWAWDev2 <dev2@marvelousworkandawonder.com>
2018-03-06 16:31:30 -0700

    (feat) add LargeCard component

---
+++ packages/Frontend/src/components/LargeCard.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { Card, Icon, Image } from 'semantic-ui-react';
+
+export default class LargeCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      starFilled: false,
+    };
+  }
+  toggleFav = () => this.setState({ starFilled: !this.state.starFilled })
+  render() {
+    const { starFilled } = this.state;
+    return (
+      <div style={{ position: 'relative', display: 'inline' }}>
+        <Card>
+          <Icon
+            name="star"
+            color={starFilled ? 'yellow' : 'whitesmoke'}
+            onClick={this.toggleFav}
+            size="huge"
+            style={{
+              color: 'whitesmoke',
+              position: 'absolute',
+              zIndex: 1,
+              right: 0,
+              top: 9,
+            }}
+          />
+          <Image />
+          <Card.Content>
+            <Card.Header>
+              The Sealed Portion
+            </Card.Header>
+          </Card.Content>
+          <Card.Content extra>
+            <p>
+              <Icon name="file text outline" />
+              22 hr read
+            </p>
+          </Card.Content>
+        </Card>
+      </div>
+    );
+  }
+}
