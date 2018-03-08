bc18dff2e4f11e4b8e3587cb1c73729d74218e0e
MWAW Developer <dev@marvelousworkandawonder.com>
2018-03-08 08:46:54 -0700

    (feat) add color, background-color, remove indenting

--- packages/Frontend/src/components/Editor.jsx
+++ packages/Frontend/src/components/Editor.jsx
@@ -37,7 +37,8 @@ const modules = {
   toolbar: [
     [{ header: [1, 2, false] }],
     ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-    [{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }],
+    [{ list: 'ordered' }, { list: 'bullet' }],
+    [{ color: [] }, { background: [] }],
     ['link', 'image'],
     ['clean'],
   ],
