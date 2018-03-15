a3d853e540c6e32eca4fdc9b2f264151fd27d6d9
MWAWDev2 <dev2@marvelousworkandawonder.com>
2018-03-15 16:21:58 -0600

    (feat) Combine LargeCard component and SmallCard component

--- packages/Frontend/src/components/Card.jsx
+++ packages/Frontend/src/components/Card.jsx
@@ -1,37 +1,69 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Card as SCard, Icon, Image } from 'semantic-ui-react';
+import styled from 'styled-components';
+import { Divider, Header, Icon, Image, Segment } from 'semantic-ui-react';
+import Placeholder from './assets/Placeholder.png';
+
+const StyledSegment = styled(Segment)`
+  @media (max-width: 600px) {
+    height: 150px;
+  }
+`;
+
+const StyledDiv = styled.div`
+  @media (max-width: 600px) {
+    margin-left: 142px;
+    margin-right: 17px;
+    position: absolute;
+    top: 40px;
+    margin-right: 10%;
+  }
+`;
+const StyledImage = styled(Image)`
+  object-fit: cover;
+
+  @media (max-width: 600px) {
+    position: absolute;
+    top: -14px;
+    left: -14px;
+    width: 137px;
+    height: 150px;
+  }
+`;
 
 export default function Card(props) {
   return (
-    <div style={{ position: 'relative', display: 'inline' }}>
-      <SCard>
-        <Icon
-          name="star"
-          onClick={props.toggleFav}
-          size="huge"
-          style={{
-            color: props.starFilled ? 'yellow' : 'whitesmoke',
-            position: 'absolute',
-            zIndex: 1,
-            right: 0,
-            top: 9,
-          }}
-        />
-        <Image />
-        <SCard.Content>
-          <SCard.Header>
+    <StyledSegment>
+      <Icon
+        name="star"
+        onClick={props.toggleFav}
+        size="huge"
+        style={{
+          color: props.starFilled ? 'yellow' : 'whitesmoke',
+          position: 'absolute',
+          zIndex: 1,
+          right: 0,
+          top: 9,
+        }}
+      />
+      <StyledImage src={Placeholder} />
+      <StyledDiv>
+        <p>
+          <Header>
             Title
-          </SCard.Header>
-        </SCard.Content>
-        <SCard.Content extra>
-          <p>
-            <Icon name="file text outline" />
-              Read Time
-          </p>
-        </SCard.Content>
-      </SCard>
-    </div>
+          </Header>
+          Short description goes here.
+        </p>
+        <Divider />
+        <p style={{ display: 'inline-block' }} >
+          <Icon name="file text outline" />
+          Read Time
+        </p>
+        <p style={{ display: 'inline-block', float: 'right' }}>
+          06/16/18
+        </p>
+      </StyledDiv>
+    </StyledSegment>
   );
 }
 
