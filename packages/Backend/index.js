ce9acb481637c4825f38d929c21d6ce0af134c6c
MWAW Developer <dev@marvelousworkandawonder.com>
2018-03-31 00:35:59 -0600

    (feat) add environmental variables via webpack

--- packages/Backend/index.js
+++ packages/Backend/index.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const functions = require('firebase-functions');
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
--- packages/Backend/webpack.config.js
+++ packages/Backend/webpack.config.js
@@ -1,7 +1,10 @@
+require('dotenv').config();
 const path = require('path');
+const webpack = require('webpack');
 const nodeExternals = require('webpack-node-externals');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
+const PRODUCTION = 'production';
 module.exports = {
   entry: './index.js',
   externals: [nodeExternals()],
@@ -10,6 +13,7 @@ module.exports = {
     libraryTarget: 'commonjs',
     path: path.resolve(__dirname, 'dist'),
   },
+  mode: PRODUCTION,
   plugins: [
     new CopyWebpackPlugin([
       {
@@ -17,7 +21,14 @@ module.exports = {
         to: '',
         toType: 'file',
       },
-    ], { debug: 'debug' }),
+    ]),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(PRODUCTION),
+      'process.env.PG_DATABASE': JSON.stringify(process.env.PG_DATABASE),
+      'process.env.PG_USER': JSON.stringify(process.env.PG_USER),
+      'process.env.PG_PASSWORD': JSON.stringify(process.env.PG_PASSWORD),
+      'process.env.PG_HOST': JSON.stringify(process.env.PG_HOST),
+    }),
   ],
   target: 'node',
 };
