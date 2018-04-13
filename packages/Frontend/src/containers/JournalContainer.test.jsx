167e3f3ec6606da6779dd97f629f3fa439784ee9
MWAW Developer <dev@marvelousworkandawonder.com>
2018-04-12 20:15:15 -0600

    (style) remove unused imports

--- packages/Frontend/src/containers/JournalContainer.test.jsx
+++ packages/Frontend/src/containers/JournalContainer.test.jsx
@@ -1,6 +1,4 @@
 /* eslint-env jest */
-import React from 'react';
-import { shallow } from 'enzyme';
 import { createAssertWithPropsToMatchSnapshot } from '../../test-utils';
 import JournalContainer from './JournalContainer';
 
