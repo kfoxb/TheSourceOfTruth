00cf4631cf223d86c5eeb3f5a1509b9e786e8a01
MWAWDev2 <dev2@marvelousworkandawonder.com>
2018-05-29 16:33:02 -0600

    (style) Fix linting errors

--- packages/Frontend/src/components/Dialog.jsx
+++ packages/Frontend/src/components/Dialog.jsx
@@ -3,8 +3,6 @@ import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
 import MDialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 
 export default function Dialog(props) {
