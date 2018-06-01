c776d0c626120a9c1b04e6d69e2b28649915f371
mwawdev2 <34819339+mwawdev2@users.noreply.github.com>
2018-06-01 10:16:44 -0600

    (feat) Style 404 page (#153)
    This commit will add styling to our 404 page.

---
+++ packages/Frontend/src/components/NotFound.test.jsx
@@ -0,0 +1,9 @@
+/* eslint-env jest */
+import { createAssertWithPropsToMatchSnapshot } from '../../test-utils';
+import NotFound from './NotFound';
+
+describe('NotFound', () => {
+  it('should render', () => {
+    createAssertWithPropsToMatchSnapshot(NotFound, {})();
+  });
+});
