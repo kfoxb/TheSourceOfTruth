dc5950932c41d72bee229e0228cd7d276d937653
mwawdev2 <34819339+mwawdev2@users.noreply.github.com>
2018-06-02 09:57:16 -0600

    (feat) Create ConnectionError for live updates (#167)
    Create ConnectionError for live updates

--- packages/Frontend/src/components/App.jsx
+++ packages/Frontend/src/components/App.jsx
@@ -3,11 +3,12 @@ import { Route, Switch } from 'react-router-dom';
 import { withRouter } from 'react-router';
 import PropTypes from 'prop-types';
 import { SidebarLeftOverlay, SidebarTopOverlay } from './Navigation';
+import AuthenticateContainer from '../containers/AuthenticateContainer';
+import ConnectionError from './errors/ConnectionError';
 import Home from './Home';
+import FirepadContainer from '../containers/FirepadContainer';
 import JournalsContainer from '../containers/JournalsContainer';
 import Library from './Library';
-import AuthenticateContainer from '../containers/AuthenticateContainer';
-import FirepadContainer from '../containers/FirepadContainer';
 import NotFound from '../components/NotFound';
 import Tasks from '../components/Tasks';
 import View from './View';
@@ -29,8 +30,16 @@ function App({
         <Route exact path="/" component={Home} />
         <View>
           <Route exact path="/journals" component={JournalsContainer} />
-          <Route exact path="/journals/:phase(edit|view)/:id" component={FirepadContainer} />
-          <Route exact path="/journals/:phase(create)/:id?" component={FirepadContainer} />
+          <Route
+            exact
+            path="/journals/:phase(edit|view)/:id"
+            render={props => (<ConnectionError {...props} component={FirepadContainer} />)}
+          />
+          <Route
+            exact
+            path="/journals/:phase(create)/:id?"
+            render={props => (<ConnectionError {...props} component={FirepadContainer} />)}
+          />
           <Route exact path="/library" component={Library} />
           <Route exact path="/sign(up|in)" component={AuthenticateContainer} />
           <Route exact path="/tasks" component={Tasks} />
