3ac39d3243e18448f1263bd76b8d2d3b7f59f719
MWAW Developer <dev@marvelousworkandawonder.com>
2018-06-02 09:51:37 -0600

    (style) remove console.logs from tasks/index.js

--- packages/Backend/src/tasks/index.js
+++ packages/Backend/src/tasks/index.js
@@ -3,7 +3,6 @@ import { database as databaseAdmin } from 'firebase-admin';
 import handleSubmit from '../journal/handleSubmit';
 
 const handleFailure = (error, docRef) => {
-  console.log('in handleFailure');
   const errorObj = {};
   ['code', 'message', 'stack'].forEach((key) => {
     const val = error[key];
@@ -16,27 +15,19 @@ const handleFailure = (error, docRef) => {
   });
 };
 
-const finish = (docRef, context) => {
-  console.log('in finish');
-  return docRef
-    .update({
-      completedTime: new Date(databaseAdmin.ServerValue.TIMESTAMP),
-      startTime: new Date(Date.parse(context.timestamp)),
-    });
-};
+const finish = (docRef, context) => docRef
+  .update({
+    completedTime: new Date(databaseAdmin.ServerValue.TIMESTAMP),
+    startTime: new Date(Date.parse(context.timestamp)),
+  });
 
 const handleTask = databaseFunction
   .ref('/tasks/{task}')
   .onCreate((snapshot, context) => {
-    console.log('in handleTask');
-    console.log('context.timestamp', context.timestamp);
-    console.log('new date', new Date(context.timestamp));
     const taskId = context.params.task;
     const { payload, type } = snapshot.val();
     const docRef = databaseAdmin().ref(`/tasks/${taskId}`);
-    console.log('checking type');
     if (type === 'submit') {
-      console.log('type is submit');
       return handleSubmit(payload, context)
         .catch(error => handleFailure(error, docRef))
         .finally(() => finish(docRef, context));
