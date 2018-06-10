3d2cfaaf3aa72abf7b596d5da7aeda5c9b10faf9
mwawdev2 <dev2@marvelousworkandawonder.com>
2018-06-10 11:49:08 -0600

    (feat) Style coming home page

--- packages/frontend/src/components/ComingSoon.jsx
+++ packages/frontend/src/components/ComingSoon.jsx
@@ -1,10 +1,35 @@
 import React, { Fragment } from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import Divider from '@material-ui/core/Divider';
+import colors from '../constants/colors';
 
 const height = 'height: 200px;';
 const width = 'width: 200px;';
 const absolute = 'position: absolute;';
+
+
+const StyledDiv = styled.div`
+  background-color: ${colors.darkGrey};
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  height: 100vh;
+`;
+
+const StyledHeader = styled.div`
+  text-align: center;
+  font-size: 50px;
+  color: ${colors.white}
+  font-family: 'Noto Sans', sans-serif;
+  position: absolute;
+  margin: auto;
+  right: 0;
+  left: 0;
+  top: 25%;
+`;
 
 const TopLeft = styled.div`
   ${height}
@@ -38,18 +63,34 @@ const BottomLeft = styled.div`
 export default function ComingSoon({ addCharToKey }) {
   return (
     <Fragment>
-      <TopLeft
-        onClick={() => addCharToKey('a')}
-      />
-      <TopRight
-        onClick={() => addCharToKey('b')}
-      />
-      <BottomRight
-        onClick={() => addCharToKey('c')}
-      />
-      <BottomLeft
-        onClick={() => addCharToKey('d')}
-      />
+      <StyledDiv>
+        <StyledHeader>
+          <p
+            style={{
+              fontSize: '90px',
+              lineHeight: '87px',
+              margin: '0 0 0em',
+              padding: '20px',
+            }}
+          >
+          COMING SOON
+          </p>
+          <Divider style={{ backgroundColor: `${colors.white}` }} />
+          <p style={{ fontSize: '25px', padding: '20px' }}>June 16, 2018</p>
+        </StyledHeader>
+        <TopLeft
+          onClick={() => addCharToKey('a')}
+        />
+        <TopRight
+          onClick={() => addCharToKey('b')}
+        />
+        <BottomRight
+          onClick={() => addCharToKey('c')}
+        />
+        <BottomLeft
+          onClick={() => addCharToKey('d')}
+        />
+      </StyledDiv>
     </Fragment>
   );
 }
