3f2dde80e6c8e24769ffbab5c8169e0e12e3f6dd
MWAW Developer <dev@marvelousworkandawonder.com>
2018-06-11 16:09:26 -0600

    (style) format HomeIntroduction spacing

--- packages/frontend/src/components/HomeIntroduction.jsx
+++ packages/frontend/src/components/HomeIntroduction.jsx
@@ -1,11 +1,115 @@
 import React from 'react';
 import View from './View';
 
+/* eslint-disable max-len */
 const HomeIntroduction = React.forwardRef((props, ref) => (
   <View>
     <div ref={ref}>
       <h2 style={{ marginTop: '25px' }}>Introduction</h2>
-      <div>Before Greek authors wrote&nbsp;<i>The Pentateuch</i>&nbsp;(Greek: “<i>five books</i>”), they had produced other literary masterpieces known as the&nbsp;<i>Iliad</i>&nbsp;and the&nbsp;<i>Odyssey</i>.&nbsp;&nbsp;The hero of the&nbsp;<i>Iliad</i>&nbsp;is Achilles.&nbsp;&nbsp;The hero of the&nbsp;<i>Odyssey</i>&nbsp;is Odysseus.&nbsp;&nbsp;There were prophets and gods in both stories.&nbsp;&nbsp;Calchas and Aegyptius are examples of these prophets in the&nbsp;<i>Iliad</i>&nbsp;and the&nbsp;<i>Odyssey</i>&nbsp;stories respectively. Zeus is the main god of both. Nearly 200 years later, Greek authors had become world-renowned for their writing and storytelling abilities.</div><div><br /></div><div>By 600 BCE, the Hebrew culture had not yet developed its language skills enough to properly write its own history—stories that had been passed down orally for many generations. In an effort to consolidate the various oral accounts of their history, Ancient Hebrew tribal leaders hired Greek authors to write the Hebrew&nbsp;<i>Torah</i>, which was originally called&nbsp;<i>The Pentateuch</i>.&nbsp;&nbsp;The main hero of Torah was Abraham.&nbsp;&nbsp;The Hebrew god was&nbsp;<i>Elohim</i>, which (interpreted properly) is a plural form of a Hebrew term meaning “god.” Over the years following the publication of&nbsp;<i>The Pentaeuch</i>, Hebrew leaders used Greek authors and storytellers to elaborate and expound upon the Hebrew story.&nbsp;&nbsp;<b>This is how the Old Testament of the Bible was created.&nbsp;</b></div><div><b /><br /></div><div>Near the 4th Century C.E., Greek authors were once again called upon to create another story. This one dovetailed the Old Testament and incorporated other contemporary beliefs held by the masses who were living in the Great Roman Empire at the time.&nbsp;&nbsp;Thus, the New Testament was created.</div><div><br /></div><div>The Bible was first written in Greek, by Greek authors who were commissioned and hired to use their adroit writing and storytelling skills to tell a story.&nbsp;&nbsp;The Bible is a fictitious story.&nbsp;&nbsp;Its storyline and its characters are not any more real than its predecessors&#39;, those of the&nbsp;<i>Odyssey</i>&nbsp;and of the&nbsp;<i>Iliad</i>.&nbsp;&nbsp;However, the Bible has been and is more widely read and accepted as&nbsp;<i>real</i>&nbsp;by more people than any other book on Earth.</div><div><br /></div><div>Throughout the years that the Bible has existed, unscrupulous individuals have used it for personal profit and to control the hearts and minds of billions of people.&nbsp;&nbsp;Anyone who has stood up to these religious leaders and called the Bible out for what it really is, was killed.&nbsp;&nbsp;Later Greek authors witnessed how the Bible was being used.&nbsp;&nbsp;To counter how the story was being used, other Greek authors invented another Hebrew hero named Isaiah.&nbsp;&nbsp;Attempting to get their point across—that religion and the men who called themselves prophets and priests were corrupt—they created the book of Isaiah.</div><div><br /></div><div>The term&nbsp;<i>Marvelous Work and a Wonder</i>® was taken from the book of Isaiah:</div><div><br /></div><ul><div>“Forasmuch as this people draw near me with their mouth, and with their lips do honour me, but have removed their heart far from me, and their fear toward me is taught by the precept of men: Therefore, behold, I will proceed to do a marvellous work among this people, even a&nbsp;<b><i>marvellous work and a wonder</i></b>: for the wisdom of their wise men shall perish, and the understanding of their prudent men shall be hid.”&nbsp;&nbsp;(Isaiah 29:13–14)</div></ul><div><br /></div><div>All religion is false.&nbsp;&nbsp;The three main religions that base their beliefs on the Bible (Judaism, Christianity, and Islam) have triggered more human misery and strife than any other social causation known to humanity.&nbsp;&nbsp;Anonymous groups of individuals have always existed throughout history to counter the negative effects of religion.&nbsp;&nbsp;They have always used the religious beliefs in which the masses were indoctrinated to counter religion. They continue to do so today.&nbsp;&nbsp;</div><div><br /></div><div>In the early 19th Century, in the newly formed United States of America, a group of these individuals laid the foundation for this modern&nbsp;<i>Marvelous Work and a Wonder</i>®.</div><div><br /></div><div>This is their official website.</div>
+      <div>
+        Before Greek authors wrote&nbsp;<i>The Pentateuch</i>&nbsp;(Greek: “<i>five books</i>”),
+        they had produced other literary masterpieces known as the&nbsp;<i>Iliad</i>&nbsp;and
+        the&nbsp;<i>Odyssey</i>.&nbsp;&nbsp;The hero of the&nbsp;<i>Iliad</i>&nbsp;is
+        Achilles.&nbsp;&nbsp;The hero of the&nbsp;<i>Odyssey</i>&nbsp;is Odysseus.&nbsp;&nbsp;There
+        were prophets and gods in both stories.&nbsp;&nbsp;Calchas and Aegyptius are examples of
+        these prophets in the&nbsp;<i>Iliad</i>&nbsp;and the&nbsp;<i>Odyssey</i>&nbsp;stories
+        respectively. Zeus is the main god of both. Nearly 200 years later, Greek authors had become
+        world-renowned for their writing and storytelling abilities.
+      </div>
+      <div>
+        <br />
+      </div>
+      <div>
+        By 600 BCE, the Hebrew culture had not yet developed its language skills enough to properly
+        write its own history—stories that had been passed down orally for many generations. In an
+        effort to consolidate the various oral accounts of their history, Ancient Hebrew tribal
+        leaders hired Greek authors to write the Hebrew&nbsp;<i>Torah</i>, which was originally
+        called&nbsp;<i>The Pentateuch</i>.&nbsp;&nbsp;The main hero of Torah was
+        Abraham.&nbsp;&nbsp;The Hebrew god was&nbsp;<i>Elohim</i>, which (interpreted properly) is a
+        plural form of a Hebrew term meaning “god.” Over the years following the publication
+        of&nbsp;<i>The Pentaeuch</i>, Hebrew leaders used Greek authors and storytellers to
+        elaborate and expound upon the Hebrew story.&nbsp;&nbsp;
+        <b>
+          This is how the Old Testament of the Bible was created.&nbsp;
+        </b>
+      </div>
+      <div>
+        <b />
+        <br />
+      </div>
+      <div>
+        Near the 4th Century C.E., Greek authors were once again called upon to create another
+        story. This one dovetailed the Old Testament and incorporated other contemporary beliefs
+        held by the masses who were living in the Great Roman Empire at the time.&nbsp;&nbsp;Thus,
+        the New Testament was created.
+      </div>
+      <div>
+        <br />
+      </div>
+      <div>
+        The Bible was first written in Greek, by Greek authors who were commissioned and hired to
+        use their adroit writing and storytelling skills to tell a story.&nbsp;&nbsp;The Bible is a
+        fictitious story.&nbsp;&nbsp;Its storyline and its characters are not any more real than its
+        predecessors&#39;, those of the&nbsp;<i>Odyssey</i>&nbsp;and of the&nbsp;<i>Iliad</i>.&nbsp;&nbsp;However,
+        the Bible has been and is more widely read and accepted as&nbsp;<i>real</i>&nbsp;by more
+        people than any other book on Earth.
+      </div>
+      <div>
+        <br />
+      </div>
+      <div>
+        Throughout the years that the Bible has existed, unscrupulous individuals have used it for
+        personal profit and to control the hearts and minds of billions of people.&nbsp;&nbsp;Anyone
+        who has stood up to these religious leaders and called the Bible out for what it really is,
+        was killed.&nbsp;&nbsp;Later Greek authors witnessed how the Bible was being
+        used.&nbsp;&nbsp;To counter how the story was being used, other Greek authors invented
+        another Hebrew hero named Isaiah.&nbsp;&nbsp;Attempting to get their point across—that
+        religion and the men who called themselves prophets and priests were corrupt—they created
+        the book of Isaiah.
+      </div>
+      <div>
+        <br />
+      </div>
+      <div>
+        The term&nbsp;<i>Marvelous Work and a Wonder</i>® was taken from the book of Isaiah:
+      </div>
+      <div>
+        <br />
+      </div>
+      <ul>
+        <div>
+          “Forasmuch as this people draw near me with their mouth, and with their lips do honour me,
+          but have removed their heart far from me, and their fear toward me is taught by the
+          precept of men: Therefore, behold, I will proceed to do a marvellous work among this
+          people, even a&nbsp;
+          <b>
+            <i>marvellous work and a wonder</i>
+          </b>: for the wisdom of their wise men shall perish, and the understanding of their
+          prudent men shall be hid.”&nbsp;&nbsp;(Isaiah 29:13–14)
+        </div>
+      </ul>
+      <div>
+        <br />
+      </div>
+      <div>
+        All religion is false.&nbsp;&nbsp;The three main religions that base their beliefs on the
+        Bible (Judaism, Christianity, and Islam) have triggered more human misery and strife than
+        any other social causation known to humanity.&nbsp;&nbsp;Anonymous groups of individuals
+        have always existed throughout history to counter the negative effects of
+        religion.&nbsp;&nbsp;They have always used the religious beliefs in which the masses were
+        indoctrinated to counter religion. They continue to do so today.&nbsp;&nbsp;
+      </div>
+      <div>
+        <br />
+      </div>
+      <div>
+        In the early 19th Century, in the newly formed United States of America, a group of these
+        individuals laid the foundation for this modern&nbsp;<i>Marvelous Work and a Wonder</i>®.
+      </div>
+      <div>
+        <br />
+      </div>
+      <div>This is their official website.</div>
     </div>
   </View>
 ));
