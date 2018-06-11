3397dcb32910acc91724d1efeb792e4dcee3aa4f
mwawdev2 <dev2@marvelousworkandawonder.com>
2018-06-11 15:49:26 -0600

    (feat) Add truncate to Titles in TasksCard

--- packages/frontend/src/components/TasksCard.jsx
+++ packages/frontend/src/components/TasksCard.jsx
@@ -3,10 +3,11 @@ import PropTypes from 'prop-types';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 import { Link } from 'react-router-dom';
 import { withStyles } from '@material-ui/core/styles';
+import { truncate } from 'lodash';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
+import Divider from '@material-ui/core/Divider';
 import Edit from '@material-ui/icons/Edit';
 import { DOCUMENTS, PHASE, VIEW } from '@the-source-of-truth/shared/constants';
 import colors from '../constants/colors';
@@ -15,10 +16,6 @@ const styles = {
   card: {
     maxheight: 265,
     maxwidth: 240,
-  },
-  media: {
-    height: 0,
-    paddingTop: '56.25%',
   },
 };
 
@@ -29,19 +26,19 @@ function TasksCard({ doc, classes, id }) {
   const editHref = `/${DOCUMENTS}/${phase}/${id}`;
   return (
     <div key={id} >
-      <Card className={classes.card}>
-        <CardMedia className={classes.media} image="https://firebasestorage.googleapis.com/v0/b/thesourceoftruth-28554.appspot.com/o/placeholder.png?alt=media&token=1c1086cd-757f-4b09-b439-772eeed00f68" />
-        <CardContent>
-          <Link to={viewHref} href={viewHref} style={{ color: `${colors.darkGrey}` }}>
-            {title || 'Untitled'}
-          </Link>
-        </CardContent>
-        <CardActions style={{ justifyContent: 'flex-end' }}>
-          <Link to={editHref} href={editHref} style={{ color: `${colors.darkGrey}` }}>
-            <Edit />
-          </Link>
-        </CardActions>
-      </Card>
+      <Link to={viewHref} href={viewHref}>
+        <Card className={classes.card}>
+          <CardContent style={{ color: `${colors.darkGrey}`, fontWeight: 'bold' }}>
+            {truncate(title, { length: 70 }) || 'Untitled'}
+          </CardContent>
+          <Divider />
+          <CardActions style={{ justifyContent: 'flex-end' }}>
+            <Link to={editHref} href={editHref} style={{ color: `${colors.darkGrey}` }}>
+              <Edit />
+            </Link>
+          </CardActions>
+        </Card>
+      </Link>
     </div>
   );
 }
