8276d15572b795a020d2735c6807d27fd3c37ae2
mwawdev2 <dev2@marvelousworkandawonder.com>
2018-06-12 01:27:45 -0600

    (fix) set uploading to false after successful upload

--- packages/frontend/src/containers/ImageUploader.jsx
+++ packages/frontend/src/containers/ImageUploader.jsx
@@ -34,28 +34,21 @@ export default class ImageUploader extends Component {
   handleImage = (files) => {
     if (files.length) {
       this.setState({ uploading: true }, () => {
-        console.log('1');
         const file = files[0];
-        console.log('1.5');
         const name = file.name
           // remove [, ], *, ?, per Google's object name guidelines
           .replace(/#|\[|\]|\*|\?/g, '')
           // replace any groups of whitespace with _
           .replace(/\s+/g, '_');
-        console.log('1.76');
         const imagesRef = storage().ref().child(`images/${Date.now()}_${name}`);
-        console.log('1.9');
         imagesRef
           .put(file)
           .then((snapshot) => {
-            console.log('2');
             return snapshot.ref.getDownloadURL()
           })
           .then((downloadURL) => {
-            console.log('3');
             this.props.firepadInst.insertEntity('img', { src: downloadURL });
-            // this.setState({ uploading: false }, this.closeModal);
-            this.closeModal();
+            this.setState({ uploading: false }, this.closeModal);
           });
       });
 
