53620efafd57b4fce1b0a46fbca2ca3effb74f3e
mwawdev2 <dev2@marvelousworkandawonder.com>
2018-06-12 14:40:40 -0600

    (feat) Add button className to match firepad image

--- packages/frontend/src/components/ImageUploaderButton.jsx
+++ packages/frontend/src/components/ImageUploaderButton.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import Button from '@material-ui/core/Button';
-import Image from '@material-ui/icons/Image';
 
 export default function ImageUploaderButton({ uploadButtonElement, launchImageUploadModal }) {
   if (uploadButtonElement) {
     return ReactDom.createPortal(
       (
-        <Button onClick={launchImageUploadModal} className="firepad-btn">
-          <Image />
-        </Button>
+        <button onClick={launchImageUploadModal} className="firepad-btn firepad-tb-insert-image" />
       ),
       uploadButtonElement,
     );
