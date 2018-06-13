7b92b72ff8bab3888d783a6a734abf3b5ebf4649
MWAW Developer <dev@marvelousworkandawonder.com>
2018-06-12 21:25:39 -0600

    (feat) add real time summaries to cards

--- packages/frontend/src/components/TasksCard.jsx
+++ packages/frontend/src/components/TasksCard.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { database } from 'firebase';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 import { withRouter } from 'react-router';
 import { withStyles } from '@material-ui/core/styles';
@@ -13,7 +14,11 @@ import Edit from '@material-ui/icons/Edit';
 import IconButton from '@material-ui/core/IconButton';
 import { DOCUMENTS, PHASE, VIEW } from '@the-source-of-truth/shared/constants';
 import { checkPermissions } from '@the-source-of-truth/shared/helpers';
+import CodeMirror from 'codemirror';
 import colors from '../constants/colors';
+
+global.CodeMirror = CodeMirror;
+const { Headless } = require('firepad/dist/firepad');
 
 const styles = {
   card: {
@@ -22,48 +27,67 @@ const styles = {
   },
 };
 
-function TasksCard({
-  doc,
-  classes,
-  history,
-  id,
-  claims,
-}) {
-  const title = doc.get('title');
-  const phase = doc.get(PHASE);
-  const viewHref = `/${DOCUMENTS}/${VIEW}/${id}`;
-  const editHref = `/${DOCUMENTS}/${phase}/${id}`;
-  const desc = '';
-  const hasPermissions = checkPermissions(claims, phase);
+class TasksCard extends Component {
+  constructor(props) {
+    super(props);
+    this.ref = database().ref(DOCUMENTS).child(props.id);
+    this.state = {
+      summary: '',
+    };
+  }
 
-  return (
-    <div key={id} >
-      <Card className={classes.card}>
-        <Button
-          onClick={() => { history.push(viewHref); }}
-          style={{
+  componentDidMount() {
+    this.headless = new Headless(this.ref);
+    this.ref.on('value', () => {
+      this.headless.getText((text) => {
+        this.setState({ summary: text });
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    this.ref.off();
+    this.headless.dispose();
+  }
+
+  render() {
+    const {
+      doc, classes, history, id, claims,
+    } = this.props;
+    const title = doc.get('title');
+    const phase = doc.get(PHASE);
+    const viewHref = `/${DOCUMENTS}/${VIEW}/${id}`;
+    const editHref = `/${DOCUMENTS}/${phase}/${id}`;
+    const hasPermissions = checkPermissions(claims, phase);
+    return (
+      <div key={id} >
+        <Card className={classes.card}>
+          <Button
+            onClick={() => { history.push(viewHref); }}
+            style={{
             height: '100%',
             padding: '0',
             textAlign: 'left',
             width: '100%',
         }}
-        >
-          <CardContent style={{ color: `${colors.darkGrey}` }}>
-            <h4 style={{ fontSize: '16px' }}>{ truncate(title, { length: 90 }) || 'Untitled'}</h4>
-            <p>{truncate(desc, { length: 170 })}</p>
-          </CardContent>
-        </Button>
-        <Divider />
-        <CardActions style={{ justifyContent: 'flex-end' }}>
-          { hasPermissions &&
+          >
+            <CardContent style={{ color: `${colors.darkGrey}` }}>
+              <h4 style={{ fontSize: '16px' }}>{ truncate(title, { length: 90 }) || 'Untitled'}</h4>
+              <p>{truncate(this.state.summary, { length: 170 })}</p>
+            </CardContent>
+          </Button>
+          <Divider />
+          <CardActions style={{ justifyContent: 'flex-end' }}>
+            { hasPermissions &&
             <IconButton onClick={() => { history.push(editHref); }} style={{ color: `${colors.darkGrey}`, height: '35px', width: '35px' }} >
               <Edit />
             </IconButton>
           }
-        </CardActions>
-      </Card>
-    </div>
-  );
+          </CardActions>
+        </Card>
+      </div>
+    );
+  }
 }
 
 TasksCard.propTypes = {
