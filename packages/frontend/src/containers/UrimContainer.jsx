b321f2a5d1b636a3389f31e5942f4a49689da28f
MWAW Developer <dev@marvelousworkandawonder.com>
2018-06-14 16:38:53 -0600

    (feat) got quill to work collaboratively

--- packages/frontend/src/containers/UrimContainer.jsx
+++ packages/frontend/src/containers/UrimContainer.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Quill from 'quill';
 import { database } from 'firebase';
 import { DOCUMENTS } from '@the-source-of-truth/shared/constants';
+import { connect } from 'react-redux';
 import 'quill/dist/quill.snow.css';
 
 window.Quill = Quill;
@@ -18,9 +19,22 @@ const toolbarOptions = [
   ['clean'],
 ];
 
-export default class UrimContainer extends Component {
+class UrimContainer extends Component {
   componentDidMount() {
-    this.ref = database().ref(DOCUMENTS).push();
+    if (this.props.isAuthenticated) {
+      this.init();
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    if (!prevProps.isAuthenticated && this.props.isAuthenticated) {
+      this.init();
+    }
+  }
+  init() {
+    // this.ref = database().ref(DOCUMENTS).push();
+    this.ref = database().ref(DOCUMENTS).child('-LF-RIgsmWhetrn5vJBR');
+    console.log('key', this.ref.key);
     const editor = new Quill('#editor-container', {
       modules: {
         toolbar: toolbarOptions,
@@ -37,3 +51,11 @@ export default class UrimContainer extends Component {
     return (<div id="editor-container" />);
   }
 }
+
+const mapStateToProps = state => ({
+  claims: state.user.claims,
+  isAuthenticated: state.user.isAuthenticated,
+  uid: state.user.uid,
+});
+
+export default connect(mapStateToProps)(UrimContainer);
