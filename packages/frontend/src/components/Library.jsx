d18c53d3ee471f830d0e4963375992d2a49338bd
mwawdev2 <dev2@marvelousworkandawonder.com>
2018-06-15 14:44:30 -0600

    (feat) Add divider to Library

--- packages/frontend/src/components/Library.jsx
+++ packages/frontend/src/components/Library.jsx
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import styled from 'styled-components';
+import Divider from '@material-ui/core/Divider';
 import { PUBLISHED } from '@the-source-of-truth/shared/constants';
 import DocumentsContainer from '../containers/DocumentsContainer';
 
@@ -14,6 +15,7 @@ export default function Library() {
   return (
     <Fragment>
       <h3>Library</h3>
+      <Divider style={{ marginBottom: '20px', width: '100%' }} />
       <StyledDiv>
         <DocumentsContainer phase={PUBLISHED} />
       </StyledDiv>
