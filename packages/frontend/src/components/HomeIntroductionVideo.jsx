c55d566934fc2c0ad9aa8b07e21de3eec33af964
MWAW Developer <dev@marvelousworkandawonder.com>
2018-06-15 18:35:07 -0600

    (feat) embed introduction video

---
+++ packages/frontend/src/components/HomeIntroductionVideo.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function HomeIntroductionVideo() {
+  return (
+    <iframe title="Introduction Video" width="560" height="315" src="https://www.youtube.com/embed/HDA2c0PvKWQ?rel=0&amp;showinfo=0" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen />
+  );
+}
