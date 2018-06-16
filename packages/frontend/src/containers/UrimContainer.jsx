4e8e9c6923883d5dd089af09e4fd8676f48ef244
MWAW Developer <dev@marvelousworkandawonder.com>
2018-06-15 19:50:00 -0600

    (fix) date functionality

--- packages/frontend/src/containers/UrimContainer.jsx
+++ packages/frontend/src/containers/UrimContainer.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Quill from 'quill';
 import firebase, { database } from 'firebase';
 import PropTypes from 'prop-types';
-import { CREATE, DELETE, DELETED, DOCUMENTS, ENG, PHASE, PRIMARY, SUBMIT, VIEW } from '@the-source-of-truth/shared/constants';
+import { CREATE, DATE, DELETE, DELETED, DOCUMENTS, ENG, PHASE, PRIMARY, SUBMIT, VIEW } from '@the-source-of-truth/shared/constants';
 import { getNextPhase } from '@the-source-of-truth/shared/helpers';
 import { connect } from 'react-redux';
 import Editor from '../components/Editor';
@@ -228,6 +228,7 @@ class UrimContainer extends Component {
         }))
         .then(() => this.primaryRef.update({
           [PHASE]: nextPhase,
+          [DATE]: database.ServerValue.TIMESTAMP,
         }))
         .then(() => {
           this.props.history.replace(`/${ENG}/tasks`);
